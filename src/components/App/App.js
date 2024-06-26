5d8e9419289a6d85f33c53bacf9b028600ccb72a
CH-Vitalij <v.charkin2017@gmail.com>
2024-06-26 13:02:35 +0500

    переписал компонент App в классовый компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,50 +1,44 @@
-import { useState } from "react";
+import { Component } from "react";
 import NewTaskForm from "../NewTaskForm";
 import TaskList from "../TaskList";
 import Footer from "../Footer";
 
 import { formatDistanceToNow } from "date-fns";
 
-export default function App() {
-  const [tasks, setTasks] = useState({
+export default class App extends Component {
+  state = {
     todoData: [],
-  });
-
-  const [btns, setBtns] = useState({
     buttons: [
       { id: crypto.randomUUID(), isActive: true, name: "All" },
       { id: crypto.randomUUID(), isActive: false, name: "Active" },
       { id: crypto.randomUUID(), isActive: false, name: "Completed" },
     ],
-  });
+    status: "All",
+  };
 
-  const [currentFilter, setCurrentFilter] = useState("All");
-
-  function createTask(label) {
+  createTask = (label) => {
     return {
       label,
       creationTime: formatDistanceToNow(Date.now(), { includeSeconds: true }),
       done: false,
       id: crypto.randomUUID(),
     };
-  }
+  };
 
-  function handleAdded(text) {
-    const newTask = createTask(text);
-
-    setTasks(({ todoData }) => {
+  handleAdded = (text) => {
+    this.setState(({ todoData }) => {
       const newArr = structuredClone(todoData);
 
-      newArr.push(newTask);
+      newArr.push(this.createTask(text));
 
       return {
         todoData: newArr,
       };
     });
-  }
+  };
 
-  function handleDone(id) {
-    setTasks(({ todoData }) => {
+  handleDone = (id) => {
+    this.setState(({ todoData }) => {
       return {
         todoData: todoData.map((task) => {
           task = task.id === id ? { ...task, done: !task.done } : task;
@@ -53,60 +47,62 @@ export default function App() {
         }),
       };
     });
-  }
+  };
 
-  function handleDeleted(id) {
-    setTasks(({ todoData }) => {
+  handleDeleted = (id) => {
+    this.setState(({ todoData }) => {
       return { todoData: todoData.filter((task) => task.id !== id) };
     });
-  }
+  };
 
-  function handleSelected(name) {
-    setCurrentFilter(name);
-    setBtns(({ buttons }) => ({
+  handleSelected = (name) => {
+    this.setState(({ buttons }) => ({
       buttons: buttons.map((btn) => ({
         ...btn,
         isActive: btn.name === name,
       })),
+      status: name,
     }));
-  }
+  };
 
-  function handleClear() {
-    setTasks(({ todoData }) => {
+  handleClear = () => {
+    this.setState(({ todoData }) => {
       return { todoData: todoData.filter((el) => !el.done) };
     });
-  }
+  };
 
-  const getFilteredTasks = () => {
-    switch (currentFilter) {
+  getFilteredTasks = () => {
+    switch (this.state.status) {
       case "Active":
-        return tasks.todoData.filter((task) => !task.done);
+        return this.state.todoData.filter((task) => !task.done);
       case "Completed":
-        return tasks.todoData.filter((task) => task.done);
+        return this.state.todoData.filter((task) => task.done);
       default:
-        return tasks.todoData;
+        return this.state.todoData;
     }
   };
 
-  return (
-    <>
-      <header className="header">
-        <h1>todos</h1>
-        <NewTaskForm onAdded={handleAdded} />
-      </header>
-      <section className="main">
-        <TaskList
-          todos={getFilteredTasks()}
-          onDone={handleDone}
-          onDeleted={handleDeleted}
-        />
-        <Footer
-          btn={btns.buttons}
-          active={tasks.todoData.filter((task) => !task.done)}
-          onSelect={handleSelected}
-          onClear={handleClear}
-        />
-      </section>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm onAdded={this.handleAdded} />
+        </header>
+        <section className="main">
+          <TaskList
+            todos={this.getFilteredTasks()}
+            onDone={this.handleDone}
+            onDeleted={this.handleDeleted}
+          />
+          <Footer
+            btn={this.state.buttons}
+            active={this.state.todoData.filter((task) => !task.done)}
+            onSelect={this.handleSelected}
+            onClear={this.handleClear}
+          />
+        </section>
+      </>
+    );
+  }
 }
