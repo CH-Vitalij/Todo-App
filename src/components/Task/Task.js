5e998b352f2b43bb6f68a56dca431d235c3233c1
CH-Vitalij <v.charkin2017@gmail.com>
2024-06-27 16:14:05 +0500

    creationTime обновляется теперь согласно философии React

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,69 +1,45 @@
 import PropTypes from "prop-types";
-import { Component } from "react";
 import { formatDistanceToNow } from "date-fns";
 
-export default class Task extends Component {
-  state = {
-    date: formatDistanceToNow(this.props.creationTime, {
-      includeSeconds: true,
-    }),
-  };
+export default function Task({
+  label = "",
+  creationTime = new Date(),
+  done = false,
+  id = "",
+  onDone = () => {},
+  onDeleted = () => {},
+  onEdited = () => {},
+}) {
+  const formattedDate = formatDistanceToNow(creationTime, {
+    includeSeconds: true,
+    addSuffix: true,
+  });
 
-  static defaultProps = {
-    label: "",
-    creationTime: new Date(),
-    done: false,
-    id: "",
-    onDone: () => {},
-    onDeleted: () => {},
-    onEdited: () => {},
-  };
-
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    creationTime: PropTypes.instanceOf(Date).isRequired,
-    done: PropTypes.bool.isRequired,
-    id: PropTypes.string.isRequired,
-    onDone: PropTypes.func,
-    onDeleted: PropTypes.func,
-    onEdited: PropTypes.func,
-  };
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 10000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: formatDistanceToNow(this.props.creationTime, {
-        includeSeconds: true,
-      }),
-    });
-  }
-
-  render() {
-    const { label, done, id, onDone, onDeleted, onEdited } = this.props;
-
-    return (
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          id={id}
-          onChange={onDone}
-          checked={done}
-        />
-        <label htmlFor={id}>
-          <span className="description">{label}</span>
-          <span className="created">{`created ${this.state.date} ago`}</span>
-        </label>
-        <button className="icon icon-edit" onClick={onEdited}></button>
-        <button className="icon icon-destroy" onClick={onDeleted}></button>
-      </div>
-    );
-  }
+  return (
+    <div className="view">
+      <input
+        className="toggle"
+        type="checkbox"
+        id={id}
+        onChange={onDone}
+        checked={done}
+      />
+      <label htmlFor={id}>
+        <span className="description">{label}</span>
+        <span className="created">{`created ${formattedDate}`}</span>
+      </label>
+      <button className="icon icon-edit" onClick={onEdited}></button>
+      <button className="icon icon-destroy" onClick={onDeleted}></button>
+    </div>
+  );
 }
+
+Task.propTypes = {
+  label: PropTypes.string.isRequired,
+  creationTime: PropTypes.instanceOf(Date).isRequired,
+  done: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  onDone: PropTypes.func,
+  onDeleted: PropTypes.func,
+  onEdited: PropTypes.func,
+};
