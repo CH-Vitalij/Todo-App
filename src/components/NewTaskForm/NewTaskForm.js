23ee8bb1bba40464640474e16587e83a27087680
CH-Vitalij <v.charkin2017@gmail.com>
2024-07-25 16:00:50 +0500

    NewTaskForm -> FuncComponents

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,67 +1,49 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default class NewTaskForm extends Component {
-  state = { label: '', sec: '', min: '' };
+const NewTaskForm = ({ onAdded = () => {} }) => {
+  const [state, setState] = useState({ label: '', sec: '', min: '' });
 
-  static defaultProps = {
-    onAdded: () => {},
+  const handleLabelChange = (evt) => {
+    setState({ label: evt.target.value });
   };
 
-  static propTypes = {
-    onAdded: PropTypes.func.isRequired,
-  };
-
-  handleLabelChange = (evt) => {
-    this.setState({ label: evt.target.value });
-  };
-
-  handleSubmit = (evt) => {
+  const handleSubmit = (evt) => {
     if (evt.keyCode === 13) {
-      this.props.onAdded(
-        this.state.label,
-        this.state.min === '' ? null : this.state.min,
-        this.state.sec === '' ? null : this.state.sec,
-      );
-      this.setState({ label: '', sec: '', min: '' });
+      onAdded(state.label, state.min === '' ? null : state.min, state.sec === '' ? null : state.sec);
+      setState({ label: '', sec: '', min: '' });
     }
   };
 
-  handleSecChange = (evt) => {
+  const handleSecChange = (evt) => {
     if (!isNaN(evt.target.value)) {
-      this.setState({ sec: Number(evt.target.value) });
+      setState({ sec: Number(evt.target.value) });
     }
   };
 
-  handleMinChange = (evt) => {
+  const handleMinChange = (evt) => {
     if (!isNaN(evt.target.value)) {
-      this.setState({ min: Number(evt.target.value) });
+      setState({ min: Number(evt.target.value) });
     }
   };
 
-  render() {
-    return (
-      <form className="new-todo-form" onKeyDown={this.handleSubmit}>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-          onChange={this.handleLabelChange}
-          value={this.state.label}
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Min"
-          onChange={this.handleMinChange}
-          value={this.state.min}
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          onChange={this.handleSecChange}
-          value={this.state.sec}
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form className="new-todo-form" onKeyDown={handleSubmit}>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        onChange={handleLabelChange}
+        value={state.label}
+      />
+      <input className="new-todo-form__timer" placeholder="Min" onChange={handleMinChange} value={state.min} />
+      <input className="new-todo-form__timer" placeholder="Sec" onChange={handleSecChange} value={state.sec} />
+    </form>
+  );
+};
+
+NewTaskForm.propTypes = {
+  onAdded: PropTypes.func.isRequired,
+};
+
+export default NewTaskForm;
